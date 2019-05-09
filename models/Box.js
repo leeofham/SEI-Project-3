fdf4620a48f278a5025473b1e15373f38a8fea4f
Arjun Modi <mrarjunmodi@gmail.com>
2019-05-09 14:43:22 +0100

    corrected box

--- models/Box.js
+++ models/Box.js
@@ -1,31 +1,14 @@
 const mongoose = require('mongoose')
 
 const boxSchema = new mongoose.Schema({
-  smallBox: {
+  box: {
     type: String,
-    required: 'Please enter the small box required',
-    numberOfRare: '1',
-    numberOfCommon: '2',
-    total: '3'
-  },
-  mediumBox: {
-    type: String,
-    required: 'Please enter the medium box required',
-    numberOfLegendary: '1',
-    numberOfRare: '2',
-    numberOfCommon: '3',
-    total: '6'
-  },
-  largeBox: {
-    type: String,
-    required: 'Please enter the large box required',
-    numberOfLegendary: '2',
-    numberOfRare: '3',
-    numberOfCommon: '4',
-    total: '9'
+    required: 'What box size do you want?',
+    numberOfRare: Number,
+    numberOfCommon: Number,
+    total: Number,
+    contents: Array
   }
-
 })
-
 
 module.exports = mongoose.model('Box', boxSchema)
