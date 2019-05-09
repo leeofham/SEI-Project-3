2b8dfc69ae968cc7246ce85f89dfa464a095e9d7
Laura Murphy <laurahelenamurphy@gmail.com>
2019-05-09 14:09:49 +0100

    Updated routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,20 @@
 const router = require('express').Router()
-const productController = require('../controllers/products') 
+const productController = require('../controllers/products')
 const authController = require('../controllers/auth')
+const boxesController = require('..controllers/boxes')
 
 router.get('/', (req, res) => res.json({ message: 'Welcome to Nerd Crate API' }))
 
 router.get('/product', productController.index) // might not need this route
 router.get('/product/:id', productController.show) // might not need this route
+
+router.get('/premade', boxesController.index)
+router.get('/premade/:id', boxesController.show)
+
+router.post('/mystery/new', boxesController.create)
+router.get('/mystery/mycrate', boxesController.index)
+router.put('/mystery/mycrate/:id', boxesController.edit)
+router.delete('/mystery/mycrate/:id', boxesController.delete)
 
 router.post('/register', authController.register)
 router.post('/login', authController.login)
