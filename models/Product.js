49603ce482a2eb888282542d9b8155cc02a2f2f2
Liam Roberts <liamroberts300@gmail.com>
2019-05-09 12:15:07 +0100

    added Product.js model

---
+++ models/Product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  brandName: {
+    type: String,
+    required: 'Please enter the name of the brand eg Star Wars'
+  },
+  category: {
+    type: String,
+    required: 'Please enter Film, Game, Music or TV Show'
+  },
+  productType: {
+    type: String,
+    required: 'Please enter product type'
+  },
+  image: {
+    type: String,
+    required: 'Please add an image of the product'
+  },
+  rarity: {
+    type: String,
+    required: 'Please enter common, rare or legendary'
+  },
+  modifiedBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User'
+  },
+  createdBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User'
+  }
+})
+
+module.exports = mongoose.model('Product', productSchema)
