e6c0ce1c3414ea27c1119ad1a61e7e1c4b5dede5
Lauren Winter <lauren.fwinter@gmail.com>
2019-05-10 13:51:48 +0100

    saving changes to premade show as had to go back one step on git

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ import Navbar from './components/common/Navbar'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 
+import PremadeShow from './components/premade/Show'
+
 import 'bulma'
 import './style.scss'
 
@@ -19,6 +21,7 @@ class App extends React.Component{
         <div>
           <Navbar />
           <Switch>
+            <Route path="/premade/:id" component={PremadeShow} />
             <Route path="/register" component={Register} />
             <Route path="/login" component={Login} />
             <Route path="/" component={Home} />
