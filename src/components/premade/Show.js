f456505acedd02fd216ff426078cd528f69994e0
Lauren Winter <lauren.fwinter@gmail.com>
2019-05-10 11:36:44 +0100

    used axios to import data from api

--- src/components/premade/Show.js
+++ src/components/premade/Show.js
@@ -1,51 +1,64 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-// import axios from 'axios'
-// import Auth from '../../lib/Auth.js'
+import axios from 'axios'
 
 class Show extends React.Component {
 
+  constructor(props) {
+    super(props)
 
-}
-return (
-  <section className="section">
-    <div className="container">
-      <div className="level">
-        <div className="level-left">
-          <h1 className="title is-1">{name}</h1>
-        </div>
-        {this.canModify() &&
-          <div className="level-right">
-            <Link to={`/premade/${_id}/edit`} className="button is-primary">Edit</Link>
-            <button className="button is-danger" onClick={this.handleDelete}>Delete</button>
-          </div>
-        }
-      </div>
-      <hr />
+    this.state = {
+      premade: null
+    }
 
-      <div className="columns is-multiline">
-        <div className="column is-half-desktop is-full-tablet">
-          <figure className="image">
-            <img src={image} alt={name} />
-          </figure>
-        </div>
+  }
 
-        <div className="column is-half-desktop is-full-tablet">
-          <article>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-          </article>
-          <button>Small</button>
-          <button>Medium</button>
-          <button>Large</button>
+  componentDidMount() {
+    axios.get(`/api/premade/${this.props.match.params.id}`)
+      .then(res => this.setState({ premade: res.data}))
+  }
+
+
+  render() {
+    if(!this.state.premade) return null
+    const { name, _id, image } = this.state.premade
+
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="level">
+            <div className="level-left">
+              <h1 className="title is-1">{name}</h1>
+            </div>
+              <div className="level-right">
+              </div>
+            }
           <hr />
-          <h2 className="title is-4">Uploaded by {createdBy.username}</h2>
+
+          <div className="columns is-multiline">
+            <div className="column is-half-desktop is-full-tablet">
+              <figure className="image">
+                <img src={image} alt={name} />
+              </figure>
+            </div>
+
+            <div className="column is-half-desktop is-full-tablet">
+              <article>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+              </article>
+              <button>Small</button>
+              <button>Medium</button>
+              <button>Large</button>
+              <hr />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </section>
-)
+      </section>
+    )
+  }
+}
 
 
 export default Show
