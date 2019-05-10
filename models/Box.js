71ebe2743826a365e405b219867f825626d127c5
Liam Roberts <liamroberts300@gmail.com>
2019-05-10 11:18:53 +0100

    redid random functionand changed box type to name so users can name boxes

--- models/Box.js
+++ models/Box.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const boxSchema = new mongoose.Schema({
-  boxType: {
+  name: {
     type: String,
-    required: 'What box do you want?'
+    required: 'What box do you want call your box?'
   },
   total: {
     type: Number,
@@ -18,9 +18,23 @@ const boxSchema = new mongoose.Schema({
   numberOfCommon: {
     type: Number
   },
-  contents: {
-    type: Array
-  }
+  contents: [{ type: mongoose.Schema.ObjectId, ref: 'Product' }]
+})
+
+boxSchema.pre('save', function productToBox(next){
+
+  this.model('Product').find()
+    .then(products => {
+      let randomProduct = products[Math.floor(Math.random() * products.length)]
+      while(this.contents.length < this.total && !this.contents.includes(randomProduct)) {
+        this.contents.push(randomProduct)
+        randomProduct = products[Math.floor(Math.random() * products.length)]
+      }
+      next()
+
+    })
+
+    .catch(next) // handle our errors
 })
 
 module.exports = mongoose.model('Box', boxSchema)
