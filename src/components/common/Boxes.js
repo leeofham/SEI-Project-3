6fde53ca7ce32201a9325027c69580a5a74ed48f
Laura Murphy <laurahelenamurphy@gmail.com>
2019-05-10 11:19:53 +0100

    Added basic buttons

--- src/components/common/Boxes.js
+++ src/components/common/Boxes.js
@@ -6,10 +6,15 @@ class BoxesChoice extends React.Component {
 
   render() {
     return (
-      <h1 className="title is-1">Choose your box:</h1>
+      <section className="section">
+        <div className="container">
+          <h1 className="title is-1">Choose your box:</h1>
+          <button className="button is-primary">Premade Box</button>
+          <button className="button is-primary">Mystery Box</button>
+        </div>
+      </section>
     )
   }
-
 }
 
 export default BoxesChoice
