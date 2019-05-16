3e2252c919aef1d09a17dfc96bd7e925f5ee3418
Liam Roberts <liamroberts300@gmail.com>
2019-05-16 15:04:36 +0100

    dex added line to index.js

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const bodyParser = require('body-parser')
 const app = express()
 
 mongoose.connect(dbUri)
-
+app.use(express.static(`${__dirname}/dist`))
 app.use(bodyParser.json())
 app.use('/api', routes)
+
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 
 app.listen(port, () => console.log(`Nerd Crate is on port ${port}}`))
