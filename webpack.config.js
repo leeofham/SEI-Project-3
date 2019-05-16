19144d1621ec84a41ed65c3e10a42d24b7a76b1a
Liam Roberts <liamroberts300@gmail.com>
2019-05-16 15:10:42 +0100

    added file-loader to webpacks

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,16 @@ module.exports = {
     rules: [
       { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
-      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] }
+      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: './images/[name].[ext]'
+          }
+        }
+      }
     ]
   },
   devServer: {
